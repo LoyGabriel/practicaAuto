aeca224faf6bbe1de4c73462bd6e2837308b04ad
Gabriel Loy <gabrielloy01@gmail.com>
2021-01-05 21:51:43 -0300

    firstCommit

---
+++ practicaAuto/src/main/java/com/gloy/foodOverflow/PracticaAutoApplication.java
@@ -0,0 +1,13 @@
+package com.gloy.foodOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaAutoApplication.class, args);
+	}
+
+}
---
+++ practicaAuto/src/test/java/com/gloy/foodOverflow/PracticaAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gloy.foodOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
